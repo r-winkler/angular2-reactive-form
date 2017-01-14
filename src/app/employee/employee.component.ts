ff4acb4290009ebce6dc08f7679060d86471b902
René Winkler <rene.winkler@xovis.com>
2017-01-14 16:02:54 +0100

    react on changes

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Employee} from './employee.model';
 import {FormPoster} from './employee.service';
 import {FormGroup, FormControl, FormBuilder, Validators, AbstractControl} from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 /* Custom Validator */
 function primaryLanguageValidator(c: AbstractControl): {[key: string]:boolean} | null {
@@ -18,9 +20,13 @@ function primaryLanguageValidator(c: AbstractControl): {[key: string]:boolean} |
 })
 export class EmployeeComponent implements OnInit {
     employeeForm: FormGroup;   
-    model : Employee;
-        
+    model : Employee;  
+    firstNameMessage: string;
     languages = [];
+    private validationMessages = {
+        required: 'Firstname is required.',
+        minlength: 'Firstname must be at least 3 characters.'
+    };
     
     constructor(private formPoster: FormPoster, private fb: FormBuilder) {
         this.formPoster.getLanguages()
@@ -49,16 +55,21 @@ export class EmployeeComponent implements OnInit {
             primaryLanguage: ['default', primaryLanguageValidator]         
         });
         
-    }  
+        const lastNameControl = this.employeeForm.get('lastName');
+        lastNameControl.valueChanges.distinctUntilChanged().subscribe(value => this.lastNameToUpperCase(lastNameControl.value));
+        
+        const firstNameControl = this.employeeForm.get('firstName');
+        firstNameControl.valueChanges.debounceTime(1000).subscribe(value => this.setMessage(firstNameControl));
+        
+    }
     
     lastNameToUpperCase(value: string) {
         if(value.length > 0) 
-            this.model.lastName = value.charAt(0).toUpperCase() + value.slice(1);
+            this.employeeForm.patchValue({lastName: value.charAt(0).toUpperCase() + value.slice(1)});
         else
-            this.model.lastName = value;
+            this.employeeForm.patchValue({lastName: value});
     }
-    
-    
+        
     submitForm() {
         console.log('Saved: ' + JSON.stringify(this.employeeForm.value));
         console.log(this.employeeForm);
@@ -68,6 +79,13 @@ export class EmployeeComponent implements OnInit {
                 data => console.log('success:', data),
                 err => console.log('error:', err)
             ); 
+    }
+    
+    setMessage(c: AbstractControl): void {
+        this.firstNameMessage = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.firstNameMessage = Object.keys(c.errors).map(key => this.validationMessages[key]).join(' ');
+        }
     }
     
 }
